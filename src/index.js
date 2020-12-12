0a9de80aca6f584d9ea9b51b0973d52870400c9c
mchengat <mchengat@verizonmedia.com>
2020-12-12 22:38:29 +0530

    3rd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 //module.exports = function fetchMovies () {};
 
-import {fetchWithTimeout} from "./services";
+import {fetchBooks, fetchMovies, fetchWithTimeout} from "./services";
 
 const movies = require("./data/movies.json")
-//const services = require('./services')
-export function fetchMovies() {
-    const resolveFunction = () => movies
-    return fetchWithTimeout(1000).then(resolveFunction)
+
+const getBooksAndMovies = () => {
+    return Promise.all([fetchBooks(), fetchWithTimeout()])
+        .then(([books, movies ]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
 }
 
-let moviePromise = fetchMovies()
-moviePromise.then((results) => {console.log(results)})
+const getBooksAndMoviesPromise = getBooksAndMovies()
+getBooksAndMoviesPromise.then((results) => {
+    console.log('getBooksAndMoviesPromise', results)
+})
+
+const getBooksOrMovies = () => {
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies()
+getBooksOrMoviesPromise.then((results) => {console.log('getBooksOrMoviesPromise', results)})
+
+
+//const services = require('./services')
+// export function fetchMovies() {
+//     const resolveFunction = () => movies
+//     return fetchWithTimeout(1000).then(resolveFunction)
+// }
+//
+// let moviePromise = fetchMovies()
+// moviePromise.then((results) => {console.log(results)})
