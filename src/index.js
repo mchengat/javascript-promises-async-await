610f49e4623ba34122f0297a531783d41f0e4ae9
mchengat <mchengat@verizonmedia.com>
2020-12-12 23:12:16 +0530

    3rd

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ async function getBooksAndMoviesAsync() {
         return {books, movies}
     }
     catch (error){
-        console.error("Error fetching books and movies", error)
+        console.log("Error fetching books and movies", error)
     }
 }
 
