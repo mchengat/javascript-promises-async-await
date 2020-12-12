abec4f90a956ac5418d557a3408b99adcaabfaea
mchengat <mchengat@verizonmedia.com>
2020-12-12 22:29:04 +0530

    3rd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 //module.exports = function fetchMovies () {};
 
-export function fetchMovies() {}
+import {fetchWithTimeout} from "./services";
 
+const movies = require("./data/movies.json")
+//const services = require('./services')
+export function fetchMovies() {
+    const resolveFunction = () => movies
+    return fetchWithTimeout(1000).then(resolveFunction)
+}
+
+let moviePromise = fetchMovies()
+moviePromise.then((results) => {console.log(results)})
